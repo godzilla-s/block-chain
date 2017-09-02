110b7b61de194ecb1d31449d9a2c3558508dca4d
Weijin Zhu <472214158@qq.com>
2017-09-02 23:49:46 +0800

    Update block_txn_attr.sol

--- block_txn_attr.sol
+++ block_txn_attr.sol
@@ -2,6 +2,26 @@ pragma solidity ^0.4.10;
 
 // 区块与交易属性
 contract Block_Txn_Attr {
+    // 获取当前矿工的地址
+    function getCoinbase() returns (address) {
+        return block.coinbase;
+    }
+    
+    // 当前块的难度
+    function getCurrDifficulty()  returns (uint) {
+        return block.difficulty;
+    }
+    
+    // 当前块的gaslimit
+    function getCurrGaslimit() returns (uint) {
+        return block.gaslimit;
+    }
+    
+    // 完整的调用数据（calldata）
+    function getMsgData() returns (bytes) {
+        return msg.data;
+    }
+ 
     // 获取区块hash
     function getBlockHash() returns (bytes32) {
         return block.blockhash(block.number-1);
@@ -13,12 +33,12 @@ contract Block_Txn_Attr {
     }
     
     // 获取当前地址
-    function getCurrAddr() returns (address) {
+    function getMsgSender() returns (address) {
         return msg.sender;
     }
     
-    // 获取发送者余额 注意 payable 修饰
-    function getSenderBalance() payable returns (uint) {
+    // 这个消息所附带的货币量，单位为wei
+    function getMsgValue() payable returns (uint) {
         return msg.value;
     }
     
