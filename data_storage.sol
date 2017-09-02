71c661872c645207d78a11207a6d9c4c421a47b0
Weijin Zhu <472214158@qq.com>
2017-09-02 23:39:50 +0800

    Create data_storage.sol

---
+++ data_storage.sol
@@ -0,0 +1,31 @@
+// 数据存储
+// 数据存储主要两种: memory, storage
+
+pragma solidity ^0.4.0;
+
+contract Storage {
+  uint  value_01;   // memory 存储
+  mapping (uint => uint) public value_02; // storage 存储
+  
+  function setMemoryData() {
+    value_01 = 80;
+  }
+  
+  function setStorageData() {
+    value_02[1] = 60;
+  }
+  
+  function changeMemoryData(uint a) {
+    var temp = value_01;
+    temp = a;
+  }
+  
+  function changeStorageData(uint a) {
+    var temp = value_02;
+    value02[0] = a;
+  }
+  
+  function getValue() returns(uint, uint) {
+    return (value_01, value_02[1]);
+  }
+}
