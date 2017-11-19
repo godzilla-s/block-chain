eb187fa009479910b6a73a7cb30841ea557efc60
Weijin Zhu <472214158@qq.com>
2017-11-19 23:16:31 +0800

    add

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,10 +1,15 @@
 package main 
 
 import (
+	"encoding/gob"
 	"bytes"
 	"crypto/sha256"
 	"time"
 	"strconv"
+	"log"
+
+	// 引入数据库 
+	_ "github.com/syndtr/goleveldb/leveldb"
 )
 
 // 区块 
@@ -50,6 +55,32 @@ func (b *Block) SetHash() {
 // 创世区块 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+// 将Block序列转化为字节数据
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer 
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+// 将字节数组反序列化为一个Block 
+func DeserialBlock(d []byte) *Block {
+	var block Block 
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
 
 type BlockChain struct {
---
+++ blockchain/cmd.go
@@ -0,0 +1,6 @@
+// 命令行 
+package main
+
+import (
+	_ "github.com/spf13/cobra"
+)
